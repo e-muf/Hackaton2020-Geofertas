2e0d1f17bc96e2f82202f86025a8beae3aea6942
Emanuel Flores <mufymes@gmail.com>
2020-10-18 00:03:40 -0500

    Inserciones beneficio

---
+++ BBVA-BD/SQL/11-beneficio.sql
@@ -0,0 +1,4 @@
+insert into beneficio (beneficio_id, clave, descripcion) values (1, 'KHL58BDGS', 'Viaja con tus Puntos BBVA a Europa');
+insert into beneficio (beneficio_id, clave, descripcion) values (2, 'OPH14SKRJ', 'Puntos: 1.6 Puntos BBVA = 1');
+insert into beneficio (beneficio_id, clave, descripcion) values (3, 'XMT08PSRF', 'Tus puntos valen 10% más');
+insert into beneficio (beneficio_id, clave, descripcion) values (4, 'XUR13HIZV', 'Gana hasta $400,000 con tus puntos BBVA');
